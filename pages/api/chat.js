ef912658863c9c768eb21589cd9643e36e6f7785
Alexandre Le Corre <a.elreco@gmail.com>
2022-12-17 21:32:29 +0100

    api

---
+++ pages/api/chat.js
@@ -0,0 +1,21 @@
+import { ChatGPTAPIBrowser } from 'chatgpt'
+
+const initChatGPT = async () => {
+  const api = new ChatGPTAPIBrowser({
+    email: process.env.OPENAI_EMAIL,
+    password: process.env.OPENAI_PASSWORD
+  })
+  await api.initSession()
+  return api
+}
+
+export default async function handler(req, res) {
+  /* if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' })
+    return
+  } */
+
+  const api = await initChatGPT()
+  let response = await api.sendMessage(req.body.message)
+  res.status(200).json(response.response)
+}
