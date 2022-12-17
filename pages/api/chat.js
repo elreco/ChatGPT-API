eb500848c82a424d6653cb6d7bd45cd80c77c4fb
Alexandre LE CORRE <a.elreco@gmail.com>
2022-12-17 21:59:03 +0100

    ok

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,11 +1,31 @@
-import { ChatGPTAPIBrowser } from 'chatgpt'
+import { ChatGPTAPI, getOpenAIAuth } from 'chatgpt'
 require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
+require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
+require('puppeteer-extra-plugin-stealth/evasions/defaultArgs');
+require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
+require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
+require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
+require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
+require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
+require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
+require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
+require('puppeteer-extra-plugin-user-preferences');
+require('puppeteer-extra-plugin-user-data-dir');
 
 const initChatGPT = async () => {
-  const api = new ChatGPTAPIBrowser({
+  const openAIAuth = await getOpenAIAuth({
     email: process.env.OPENAI_EMAIL,
     password: process.env.OPENAI_PASSWORD
   })
+
+  const api = new ChatGPTAPI({ ...openAIAuth })
   await api.initSession()
   return api
 }
